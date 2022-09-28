a0384414368157f400011a338c89b7dc4a9a3fa5
deepamaddala <91093325+deepamaddala@users.noreply.github.com>
2022-09-28 20:59:27 +0000

    indentation for long sql statements

--- queries/01_02/OrderOfPrecedence.sql
+++ queries/01_02/OrderOfPrecedence.sql
@@ -1,3 +1,9 @@
-SELECT empno,name,deptno,salary FROM emp_tab WHERE deptno=30 OR deptno=40 AND salary>2500;
+SELECT empno,name,deptno,salary 
+FROM emp_tab 
+WHERE deptno=30 OR deptno=40 
+AND salary>2500;
 
-SELECT empno,name,deptno,salary FROM emp_tab WHERE (deptno=30 OR deptno=40) AND salary>2500;
+SELECT empno,name,deptno,salary 
+FROM emp_tab 
+WHERE (deptno=30 OR deptno=40) 
+AND salary>2500;

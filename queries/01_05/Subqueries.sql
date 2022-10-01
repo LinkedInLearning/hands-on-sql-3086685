7a80fcdc8a9d6cc878e01fdf2c07d8b2d7e05391
deepamaddala <91093325+deepamaddala@users.noreply.github.com>
2022-10-01 13:13:29 +0000

    ADded comments

--- queries/01_05/Subqueries.sql
+++ queries/01_05/Subqueries.sql
@@ -1,9 +1,11 @@
+--Fetches employees who belong to the same department as empno 7001
 SELECT empno,name,deptno,salary FROM emp_tab 
 WHERE deptno=(
   SELECT deptno 
   FROM emp_tab 
   WHERE empno=7001);
 
+--Fetches all states from states_tab which belong to the same country as for California
 SELECT * FROM states_tab 
 WHERE country_id=(
   SELECT country_id 
@@ -13,23 +15,22 @@ WHERE country_id=(
 SELECT * FROM states_tab 
 WHERE country_id=(1); 
 
+--Fetches the country details for the country with same country_id as for California
 SELECT * FROM country_tab 
 WHERE country_id=(
   SELECT country_id 
   FROM states_tab 
   WHERE state_name='CALIFORNIA');
 
+--Using group functions
 SELECT empno ,name, salary 
 FROM emp_tab 
 WHERE salary=(
   SELECT MAX(salary) FROM emp_tab); 
 
+--Retrieves all salaries of employees who belong to dept 30
 SELECT empno,name,salary,deptno 
 FROM emp_tab 
 WHERE salary IN (
   SELECT salary FROM emp_tab 
   WHERE deptno=30);
-
-SELECT empno,name,salary,deptno 
-FROM emp_tab 
-WHERE salary IN (2975,3000,800,1300);

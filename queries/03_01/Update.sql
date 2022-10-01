3136b00d047e18e6e91387e517483f0f8eb6d758
deepamaddala <91093325+deepamaddala@users.noreply.github.com>
2022-10-01 16:24:02 +0000

    update statements

---
+++ queries/03_01/Update.sql
@@ -0,0 +1,27 @@
+--Modify one row in emp_tab
+UPDATE emp_tab 
+SET salary=8000 
+WHERE empno=7001;
+
+--Modify multiple columns using subquery
+UPDATE emp_tab 
+SET manager = (
+  SELECT manager 
+  FROM emp_tab 
+  WHERE empno=7001), 
+salary = (
+  SELECT salary 
+  FROM emp_tab 
+  WHERE empno=7001) 
+WHERE empno=7002;
+
+--Modify row in a different table using subquery
+UPDATE old_emp_tab 
+SET deptno = (
+  SELECT deptno 
+  FROM emp_tab 
+  WHERE empno=7001) 
+WHERE empno=7001;
+
+--Integrity constraint error
+Update emp_tab SET deptno=500 WHERE deptno=10;

0266be836c8efb43cf03d147fbd9bd4d220635e5
deepamaddala <91093325+deepamaddala@users.noreply.github.com>
2022-10-01 16:07:34 +0000

    delete statements

---
+++ queries/02_04/Delete.sql
@@ -0,0 +1,16 @@
+--Delete one row from a table
+SELECT * FROM emp_tab;
+
+DELETE FROM emp_tab WHERE empno=7007;
+
+--Delete rows based on another table
+SELECT * FROM old_emp_tab;
+
+DELETE FROM old_emp_tab 
+WHERE salary=(
+  SELECT salary 
+  FROM emp_tab 
+  WHERE salary<1000);
+
+--Integrity constraint error
+DELETE FROM dept_tab WHERE deptno=10;
